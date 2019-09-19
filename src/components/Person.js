dbf5be04c327506124b1d59f5dea250ef0cb3f48
Aaron Holland <aaronholla@gmail.com>
2019-09-18 21:23:17 -0400

    added name, picture, and placeholder image with loading state to person

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import Placeholder from "../images/placeholder.png"
 
 class Person extends Component {
   constructor(props){
@@ -9,9 +10,16 @@ class Person extends Component {
   }
 
   render(){
+    const { name, picture, pending } = this.props
     const {timeSinceArrival} = this.state
     return (
       <div className="person">
+        {pending ? (
+          <img src={Placeholder} alt="Placeholder" />
+        ) : (
+          <img src={picture} alt={name} />
+        )}
+        <h3>{name || "Loading..."}</h3>
         <h4>{timeSinceArrival}</h4>
       </div>
     )
