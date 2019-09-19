2913f022b2d5c0f779ba020b51ec3648c89958a8
Aaron Holland <aaronholla@gmail.com>
2019-09-18 21:44:38 -0400

    added luxon and set arrivaltime in state

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react"
+import { DateTime } from 'luxon'
 import Placeholder from "../images/placeholder.png"
 
 class Person extends Component {
   constructor(props){
     super(props)
     this.state = {
+      arrivalTime: DateTime.local(),
       timeSinceArrival: '00:00:00'
     }
   }
